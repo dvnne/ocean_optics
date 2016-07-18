f165c3131e3b47f5a8b6714c7e1933cf1cf10068
DaVonne <dvnne@live.com>
2016-07-18 09:27:59 -0400

    still working

--- capture.py
+++ capture.py
@@ -2,42 +2,50 @@
 from __future__ import division
 import rospy
 from ocean_optics.msg import Spectrum
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 import Tkinter as tk
 import numpy as np
 
 class Reflectance(object):
     def __init__(self):
-        self.initAnimation()
+        # self.initAnimation() # later
         self.bg = None
         self.ref = None
         self.spectrum = None
 
-    def subscribe(self):
+    def getData(self): # to be used as a decorator
         rospy.init_node('subscriber')
-        # set self.callback to specific fn that needs the spectrum
         self.subscriber = rospy.Subscriber("/spectrometer/spectrum", Spectrum,
-                                            self.callback)
-        rospy.spin() # test if this code can be run without rospy.spin
+                                            self.getDataWrapper)
+        self.subscriber.unregister()
 
-    def initAnimation(self):
+    def getDataWrapper(self, data):
+        self.spectrum = data.spectrum
+        self.wavelengths = data.wavelengths
+
+    def initAnimation(self): # line length
         root = tk.Tk()
-        # Define buttons
-        darkButton = tk.Button(root, command = lambda : self.capture(mode = 'dark'))
-        lightButton = tk.Button(root, command = lambda : self.capture(mode = 'ref'))
-        specButton = tk.Button(root, command = lambda : self.capture(mode = 'sample')
+        # Buttons for capturing spectra
+        dark = tk.Button(root, command = lambda : self.capture(mode = 'dark'))
+        light = tk.Button(root, command = lambda : self.capture(mode = 'ref'))
+        spec = tk.Button(root, command = lambda : self.capture(mode = 'sample'))
+        # Buttons for displaying current spectra
+        raw = tk.Button(root, command = lambda : self.display(mode = 'raw'))
+        reflect = tk.Button(root, command = lambda : self.display(mode = 'reflect'))
         # Pack all
         darkButton.pack()
         lightButton.pack()
         specButton.pack()
 
     def capture(self, mode = 'sample'):
-        spectrum = self.spectrum # current sample
-        bg = self.bg # background
-        ref = self.ref # reference
-        wavelengths = self.wavelengths
+        # Display capture?
+        self.getData()
+        if mode.lower() in ['background', 'bg']:
+            self.bg = self.spectrum
+        elif mode.lower() in ['ref', 'referece']:
+            self.ref = self.spectrum
+        elif mode.lower() == 'sample':
         # Don't allow sample if no reference spectra
-        if mode.lower() == 'sample':
             if bg == None:
                 self.noBackgroundWarn()
                 warn = True
@@ -47,9 +55,8 @@ class Reflectance(object):
             if warn == True:
                 return
             # R = (sample - background)/(reference - background)
+            specturm, bg, ref = self.spectrum, self.bg, self.ref
             self.R = (spectrum - bg)/(ref - bg)
-            return self.R
-        return
 
     def noBackgroundWarn(self):
         print "Must take background spectrum before calculating reflectance"
@@ -57,8 +64,10 @@ class Reflectance(object):
     def noReferenceWarn(self):
         print "Must take reference spectrum before calculating reflectance"
 
-    def callback_dec(self, fn):
-        def wrapper():
-            fn = self.callback
-            self.subscribe()
-
+    def display(self, data, mode = 'raw'):
+        self.getData()
+        plt.figure()
+        wavelengths = data.wavelengths
+        spectrum = data.spectrum
+        plt.plot(wavelengths, spectrum)
+        plt.show(block = False)
